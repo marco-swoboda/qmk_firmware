5dd36fee1140b9e4918842c7c789c3b3c4c168cd
Marco Swoboda <marco.swoboda@vfst.de>
2020-08-11 18:13:22 +0200

    config.h erweitert und Debug Key aktiviert

--- keyboards/keebio/iris/keymaps/marco-swoboda/config.h
+++ keyboards/keebio/iris/keymaps/marco-swoboda/config.h
@@ -17,3 +17,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+/* Set Polling rate to 1000Hz */
+#define USB_POLLING_INTERVAL_MS 1
+
+// this makes it possible to do rolling combos (zx) with keys that
+// convert to other keys on hold (z becomes ctrl when you hold it,
+// and when this option isn't enabled, z rapidly followed by x
+// actually sends Ctrl-x. That's bad.)
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define FORCE_NKRO
+
+#ifdef TAPPING_TERM
+#    undef TAPPING_TERM
+#    define TAPPING_TERM 175
+#endif  // TAPPING_TERM
+
+/* Disable unused and unneeded features to reduce on firmware size */
+#ifdef LOCKING_SUPPORT_ENABLE
+#    undef LOCKING_SUPPORT_ENABLE
+#endif
+#ifdef LOCKING_RESYNC_ENABLE
+#    undef LOCKING_RESYNC_ENABLE
+#endif
