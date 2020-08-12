7e6a75f68649a8c29a789c1ce1ca0a0ae8cc51be
Marco Swoboda <marco.swoboda@vfst.de>
2020-08-12 17:14:56 +0200

    Nyquist Levinson angepasst

--- keyboards/keebio/nyquist/keymaps/marco-swoboda/config.h
+++ keyboards/keebio/nyquist/keymaps/marco-swoboda/config.h
@@ -1,37 +1,42 @@
+/*
+Copyright 2020 @marco-swoboda
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
-/* Use I2C or Serial, not both */
+/* Set Polling rate to 1000Hz */
+#define USB_POLLING_INTERVAL_MS 1
 
-#define USE_SERIAL
-// #define USE_I2C
+// this makes it possible to do rolling combos (zx) with keys that
+// convert to other keys on hold (z becomes ctrl when you hold it,
+// and when this option isn't enabled, z rapidly followed by x
+// actually sends Ctrl-x. That's bad.)
+#define IGNORE_MOD_TAP_INTERRUPT
 
-/* Select hand configuration */
+#define FORCE_NKRO
 
-#define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
+#ifdef TAPPING_TERM
+#    undef TAPPING_TERM
+#    define TAPPING_TERM 175
+#endif  // TAPPING_TERM
 
-#define TAPPING_TERM 150
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 12
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-
-#define AUTO_SHIFT_TIMEOUT 175
-
-#define LAYOUT_ortho_4x12_short( \
-    L00, L01, L02, L03, L04, L05, R00, R01, R02, R03, R04, R05, \
-    L10, L11, L12, L13, L14, L15, R10, R11, R12, R13, R14, R15, \
-    L20, L21, L22, L23, L24, L25, R20, R21, R22, R23, R24, R25, \
-    L30, L31, L32, L33, L34, L35, R30, R31, R32, R33, R34, R35 \
-    ) \
-    LAYOUT_ortho_5x12( \
-        KC_##L00, KC_##L01, KC_##L02, KC_##L03, KC_##L04, KC_##L05, KC_##R00, KC_##R01, KC_##R02, KC_##R03, KC_##R04, KC_##R05, \
-        KC_##L10, KC_##L11, KC_##L12, KC_##L13, KC_##L14, KC_##L15, KC_##R10, KC_##R11, KC_##R12, KC_##R13, KC_##R14, KC_##R15, \
-        KC_##L20, KC_##L21, KC_##L22, KC_##L23, KC_##L24, KC_##L25, KC_##R20, KC_##R21, KC_##R22, KC_##R23, KC_##R24, KC_##R25, \
-        KC_##L30, KC_##L31, KC_##L32, KC_##L33, KC_##L34, KC_##L35, KC_##R30, KC_##R31, KC_##R32, KC_##R33, KC_##R34, KC_##R35, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO \
-    )
+/* Disable unused and unneeded features to reduce on firmware size */
+#ifdef LOCKING_SUPPORT_ENABLE
+#    undef LOCKING_SUPPORT_ENABLE
+#endif
+#ifdef LOCKING_RESYNC_ENABLE
+#    undef LOCKING_RESYNC_ENABLE
+#endif
--- keyboards/keebio/nyquist/keymaps/marco-swoboda/keymap.c
+++ keyboards/keebio/nyquist/keymaps/marco-swoboda/keymap.c
@@ -1,247 +1,64 @@
-#include QMK_KEYBOARD_H
-#include "keymap_german.h"
-#include "sendstring_german.h"
+#include "marco-swoboda.h"
 
-extern keymap_config_t keymap_config;
+/*
+ * The `LAYOUT_iris_base` macro is a template to allow the use of identical
+ * modifiers for the default layouts (eg QWERTY, Colemak, Dvorak, etc), so
+ * that there is no need to set them up for each layout, and modify all of
+ * them if I want to change them.  This helps to keep consistency and ease
+ * of use. K## is a placeholder to pass through the individual keycodes
+ */
 
-#define _QWERTY 0
-#define _COLEMAK 1
-#define _WORKMAN 2
-#define _LOWER 3
-#define _RAISE 4
-#define _FN3 5
-#define _FN4 6
-#define _ADJUST 16
+// clang-format off
 
-enum custom_keycodes {
-    QWERTY = SAFE_RANGE,
-    COLEMAK,
-    WORKMAN,
-    LOWER,
-    RAISE,
-    FN3,
-    FN4,
-    ADJUST,
-};
+#define LAYOUT_wrapper(...)                  LAYOUT_ortho_4x12(__VA_ARGS__)
 
-#define KC_ KC_TRNS
+#define LAYOUT_levinson_base( \
+    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, \
+    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
+    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A  \
+  ) \
+  LAYOUT_wrapper( \
+    KC_ESC,         K01,        K02,     K03,     K04,     K05,           K06,      K07,     K08,     K09,     K0A,     KC_MINS,         \
+    LALT_T(KC_TAB), K11,        K12,     K13,     K14,     K15,           K16,      K17,     K18,     K19,     K1A,     KC_BSLS,         \
+    KC_TAB,         CTL_T(K21), K22,     K23,     K24,     K25,           K26,      K27,     K28,     K29,     K2A,     RALT_T(KC_QUOT), \
+    OS_LSFT,        OS_RCTL,    KC_GRV,  OS_LGUI, SP_LWER, BK_LWER,       DL_RAIS,  ET_RAIS, OS_RGUI, OS_RALT, OS_RCTL, OS_RSFT          \
+  )
 
-#define KC_CAPW LGUI(LSFT(KC_3))        // Capture whole screen
-#define KC_CPYW LGUI(LSFT(LCTL(KC_3)))  // Copy whole screen
-#define KC_CAPP LGUI(LSFT(KC_4))        // Capture portion of screen
-#define KC_CPYP LGUI(LSFT(LCTL(KC_4)))  // Copy portion of screen
-#define KC_X0 KC_ESC //MT(MOD_LCTL, KC_ESC)
-#define KC_NUM LOWER
-#define KC_SYM RAISE
-#define KC_X3 LT(_FN3, KC_GRV)
-#define KC_X4 MT(MOD_LSFT, KC_ENT)
-#define KC_X5 BL_STEP
-#define KC_X6 MT(MOD_LALT, KC_SPC)
-#define KC_X7 MT(MOD_RALT, KC_BSPC)
-#define KC_XLC MT(MOD_LSFT, KC_SPC)
-#define KC_XRC MT(MOD_RSFT, KC_BSPC)
+#define LAYOUT_levinson_base_wrapper(...)       LAYOUT_levinson_base(__VA_ARGS__)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/*  QWERTY
- * ┌────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┐
- * |   X0   |   Q    |   W    |   E    |   R    |   T    |        |   Y    |   U    |   I    |   O    |   P    |  MINS  |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  TAB   |   A    |   S    |   D    |   F    |   G    |        |   H    |   J    |   K    |   L    |  SCLN  |  QUOT  |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  LSFT  |   Z    |   X    |   C    |   V    |   B    |        |   N    |   M    |  COMM  |  DOT   |  SLSH  |   X4   |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  XLC   |  LCTL  |   X3   |  LGUI  |   X6   |  NUM   |        |  SYM   |   X7   |  EQL   |  BSLS  |   UP   |  XRC  |
- * └────────┴────────┴────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┴────────┴────────┘
- */
-  [_QWERTY] = LAYOUT_ortho_4x12_short(
-    X0  , Q   , W , E   , R , T ,      Y , U , I   , O   , P   , MINS,
-    TAB , A   , S , D   , F , G ,      H , J , K   , L   , SCLN, QUOT,
-    LSFT, Z   , X , C   , V , B ,      N , M , COMM, DOT , SLSH, X4  ,
-    XLC , LCTL, X3, LGUI, X6,NUM,     SYM, X7, EQL , BSLS, UP  , XRC
-),
+  [_QWERTY] = LAYOUT_levinson_base_wrapper (
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
+  ),
 
-/*  COLEMAK
- * ┌────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┐
- * |   X0   |   Q    |   W    |   F    |   P    |   G    |        |   J    |   L    |   U    |   Y    |  SCLN  |  MINS  |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  TAB   |   A    |   R    |   S    |   T    |   D    |        |   H    |   N    |   E    |   I    |   O    |  QUOT  |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  LSFT  |   Z    |   X    |   C    |   V    |   B    |        |   K    |   M    |  COMM  |  DOT   |  SLSH  |   X4   |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  XLC   |  LCTL  |   X3   |  LGUI  |   X6   |  NUM   |        |  SYM   |   X7   |  EQL   |  BSLS  |   UP   |  XRC  |
- * └────────┴────────┴────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┴────────┴────────┘
- */
-    [_COLEMAK] = LAYOUT_ortho_4x12_short(
-        X0    ,   Q    ,   W    ,   F    ,   P    ,   G  ,            J    ,   L    ,   U    ,   Y    ,  SCLN  ,  MINS  ,
-        TAB   ,   A    ,   R    ,   S    ,   T    ,   D  ,            H    ,   N    ,   E    ,   I    ,   O    ,  QUOT  ,
-        LSFT  ,   Z    ,   X    ,   C    ,   V    ,   B  ,            K    ,   M    ,  COMM  ,  DOT   ,  SLSH  ,   X4   ,
-        XLC   ,  LCTL  ,   X3   ,  LGUI  ,   X6   ,  NUM ,           SYM   ,   X7   ,  EQL   ,  BSLS  ,   UP   ,  XRC
-    ),
+  [_WORKMAN] = LAYOUT_levinson_base_wrapper(
+    KC_Q,    KC_D,    KC_R,    KC_W,    KC_B,    KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN,
+    KC_A,    KC_S,    KC_H,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,
+    KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH
+  ),
 
-/*  WORKMAN
- * ┌────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┐
- * |   X0   |   Q    |   D    |   R    |   W    |   B    |        |   J    |   F    |   U    |   P    |  SCLN  |  MINS  |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  TAB   |   A    |   S    |   H    |   T    |   G    |        |   Y    |   N    |   E    |   O    |   I    |  QUOT  |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  LSFT  |   Z    |   X    |   M    |   C    |   V    |        |   K    |   L    |  COMM  |  DOT   |  SLSH  |   X4   |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  XLC   |  LCTL  |   X3   |  LGUI  |   X6   |  NUM   |        |  SYM   |   X7   |  EQL   |  BSLS  |   UP   |  XRC  |
- * └────────┴────────┴────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┴────────┴────────┘
- */
-    [_WORKMAN] = LAYOUT_ortho_4x12_short(
-        X0    ,   Q    ,   D    ,   R    ,   W    ,   B    ,          J    ,   F    ,   U    ,   P    ,  SCLN  ,  MINS  ,
-        TAB   ,   A    ,   S    ,   H    ,   T    ,   G    ,          Y    ,   N    ,   E    ,   O    ,   I    ,  QUOT  ,
-        LSFT  ,   Z    ,   X    ,   M    ,   C    ,   V    ,          K    ,   L    ,  COMM  ,  DOT   ,  SLSH  ,   X4   ,
-        XLC   ,  LCTL  ,   X3   ,  LGUI  ,   X6   ,  NUM   ,         SYM   ,   X7   ,  EQL   ,  BSLS  ,   UP   ,  XRC
-    ),
+  [_LOWER] = LAYOUT_wrapper(
+     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
+     RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGDN,
+     KC_DEL,  _______, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,     KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_HOME,
+     BL_STEP, _______, _______, _______, KC_DOWN, KC_LCBR,     KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, KC_END
+  ),
 
-/*  LOWER (Zahlen) [HOLDNUM]
- * ┌────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┐
- * |   X5   |    1   |   2    |   3    |   4    |   5    |        |   6    |   7    |   8    |   9    |   0    |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  DEL   |  CAPP  |  LEFT  |  RIGHT |   UP   |   [    |        |   ]    |   P4   |   P5   |   P6   |   +    |   |    |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  MUTE  |  CPYP  |        |        |  DOWN  |   {    |        |   }    |   P1   |   P2   |   P3   |   -    |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        |        |  DEL   |        |        |        |  DEL   |   P0   |  PDOT  |        |        |
- * └────────┴────────┴────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┴────────┴────────┘
- */
-    [_LOWER] = LAYOUT_ortho_4x12_short(
-        X5  ,   1    ,   2    ,   3    ,   4    ,   5    ,            6    ,   7    ,   8    ,   9    ,   0    ,        ,
-        DEL ,  CAPP  ,  LEFT  ,   UP   ,  RGHT  ,  LBRC  ,           RBRC  ,   P4   ,   P5   ,   P6   ,  PLUS  ,  PIPE  ,
-            ,  CPYP  ,        ,  DOWN  ,        ,  LCBR  ,           RCBR  ,   P1   ,   P2   ,   P3   ,  MINS  ,        ,
-            ,        ,        ,        ,  DEL   ,        ,                 ,  DEL   ,   P0   ,  PDOT  ,        ,
-    ),
+  [_RAISE] = LAYOUT_wrapper(
+     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+     RGB_MOD, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,      KC_EQL,  KC_HOME, RGB_HUI, RGB_SAI, RGB_VAI, _______,
+     KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS,      KC_PLUS, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______
+  ),
 
-
-/*  RAISE (Sonderzeichen) [HOLDSYM]
- * ┌────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┐
- * |   ~    |    !   |   @    |   #    |   $    |   %    |        |   ^    |   &    |   *    |   (    |   )    |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  DEL   | M PREV | M NEXT | VOL UP | PAGE UP|  [_]   |        |   =    |  HOME  |        |        |        |   \    |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |  MUTE  | M STOP | M PLAY | VOL DN | PAGE DN|  [-]   |        |   +    |  END   |        |        |        |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |
- * └────────┴────────┴────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┴────────┴────────┘
- */
-    [_RAISE] = LAYOUT_ortho_4x12_short(
-        GRAVE   ,  EXLM  ,   AT   ,  HASH  ,  DLR   ,  PERC  ,           CIRC  ,  AMPR  ,  ASTR  ,  LPRN  ,  RPRN  ,        ,
-        DEL     ,  MPRV  ,  MNXT  ,  VOLU  ,  PGUP  ,  UNDS  ,           EQL   ,  HOME  ,        ,        ,        ,  BSLS  ,
-        MUTE    ,  MSTP  ,  MPLY  ,  VOLD  ,  PGDN  ,  MINS  ,           PLUS  ,  END   ,        ,        ,        ,        ,
-                ,        ,        ,        ,        ,        ,                 ,        ,        ,        ,        ,
-    ),
-
-/*  _FN (Function Layer) [HOLD X3]
- * ┌────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┐
- * |  F12   |   F1   |   F2   |   F3   |   F4   |   F5   |        |   F6   |   F7   |   F8   |   F9   |  F10   |  F11   |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |
- * └────────┴────────┴────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┴────────┴────────┘
- */
-    [_FN3] = LAYOUT_ortho_4x12_short(
-        F12 ,   F1   ,   F2   ,   F3   ,   F4   ,   F5   ,            F6   ,   F7   ,   F8   ,   F9   ,  F10   ,  F11   ,
-            ,        ,        ,        ,        ,        ,                 ,        ,        ,        ,        ,        ,
-            ,        ,        ,        ,        ,        ,                 ,        ,        ,        ,        ,        ,
-            ,        ,        ,        ,        ,        ,                 ,        ,        ,        ,        ,
-    ),
-
-/*  Adjust (Lower + Raise) (NUM +SYM)
- * ┌────────┬────────┬────────┬────────┬────────┬────────┐        ┌────────┬────────┬────────┬────────┬────────┬────────┐
- * |        | Reset  | RGB Tg | RGB Md | Hue Up | Hue Dn |        | Sat Up | Sat Dn | Val Up | Val Dn |        |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        | Aud on | Aud off| AG norm|        |AG swap | Qwerty | Colemk | Workman|        |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |
- * ├────────┼────────┼────────┼────────┼────────┼────────┤        ├────────┼────────┼────────┼────────┼────────┼────────┤
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |
- * └────────┴────────┴────────┴────────┴────────┴────────┘        └────────┴────────┴────────┴────────┴────────┴────────┘
- */
-  [_ADJUST] = LAYOUT( \
-    _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
-    _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, WORKMAN,  _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  [_ADJUST] = LAYOUT_wrapper(
+     _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+     _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,       RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______,
+     _______, _______, _______, AU_ON,   AU_OFF,  _______,       _______, QWERTY , WORKMAN, WORKMAN, DEBUG , _______,
+     _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______
   )
-
 };
-
-#ifdef AUDIO_ENABLE
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
-float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
-#endif
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_qwerty);
-        #endif
-        persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case COLEMAK:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_colemak);
-        #endif
-        persistent_default_layer_set(1UL<<_COLEMAK);
-      }
-      return false;
-      break;
-    case WORKMAN:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(tone_dvorak);
-        #endif
-        persistent_default_layer_set(1UL<<_WORKMAN);
-      }
-      return false;
-      break;
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
